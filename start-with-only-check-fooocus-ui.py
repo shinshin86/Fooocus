1c7ca2030a6c48ee8bd574434f7aab5c1ca7f92f
shinshin86 <beagles1986@gmail.com>
2023-11-22 22:08:10 +0900

    add ini_fcbh_args

--- start-with-only-check-fooocus-ui.py
+++ start-with-only-check-fooocus-ui.py
@@ -1,10 +1,18 @@
 import os
 import sys
 
+
+def ini_fcbh_args():
+    from args_manager import args
+    return args
+
+
 root = os.path.dirname(os.path.abspath(__file__))
 backend_path = os.path.join(root, 'backend', 'headless')
 sys.path += [root, backend_path]
 
 os.chdir(root)
+
+args = ini_fcbh_args()
 
 from webui import *
